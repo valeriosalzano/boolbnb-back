65955886b50b35298207c9b087b507a7861fb91c
Valerio Salzano <vlr.slzn@gmail.com>
2023-11-24 17:43:06 +0100

    basic apartment files, fixed missing user relation

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Apartment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'rooms',
+        'beds',
+        'bathrooms',
+        'square_meters',
+        'address',
+        'lat',
+        'lon',
+        'photo',
+        'visible'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function services()
+    {
+        return $this->belongsToMany(Service::class);
+
+    }
 }
