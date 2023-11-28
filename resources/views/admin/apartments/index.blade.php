9fbb0128fbde0336bc6928dda9a4341cfb591466
Valerio Salzano <vlr.slzn@gmail.com>
2023-11-28 17:38:27 +0100

    first views

---
+++ resources/views/admin/apartments/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('Appartamenti') }}
+        </h2>
+        <div class="container">
+            <div class="card">
+                <div class="card-header">{{ __('I tuoi appartamenti') }}</div>
+                <div class="card-body px-0">
+                    <table class="table table-striped">
+                        <thead>
+                          <tr class="text-center">
+                            <th scope="col" class="ps-3">Visibile</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Indirizzo</th>
+                            <th scope="col">Azioni</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($apartments as $apartment )
+                            <tr>
+                              <th scope="row" class="ps-3 text-center"><i class="fa-solid fa-eye{{$apartment->visible?'':'-slash'}}"></i></th>
+                              <td>{{$apartment->name}}</td>
+                              <td>{{$apartment->address}}</td>
+                              <td><a href="#" class="btn btn-info">Azione</a></td>
+                            </tr>                               
+                            @endforeach
+                        </tbody>
+                      </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
