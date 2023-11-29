be9ca7c57ca6b42fd3e7a84738cfe6dada02e9e0
FedericaMammone <federicakr97@gmail.com>
2023-11-29 16:08:19 +0100

    aggiunto un commento

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -10,62 +10,66 @@ use Illuminate\Support\Facades\Auth;
 class ApartmentController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Mostra un elenco delle risorse.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $apartments = Apartment::where('user_id',Auth::id())->orderBy('name','asc')->get();
-        
+        // Recupera gli appartamenti di proprietà dell'utente autenticato e li ordina per nome
+        $apartments = Apartment::where('user_id', Auth::id())->orderBy('name', 'asc')->get();
+
+        // Mostra l'elenco degli appartamenti nella vista
         return view('admin.apartments.index', compact('apartments'));
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Mostra il modulo per la creazione di una nuova risorsa.
      *
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
+        // Mostra il modulo per la creazione di un nuovo appartamento
         return view('admin.apartments.create');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Archivia una nuova risorsa appena creata.
      *
      * @param  \App\Http\Requests\StoreApartmentRequest  $request
      * @return \Illuminate\Http\Response
      */
     public function store(StoreApartmentRequest $request)
     {
-        //
+        // Logica per archiviare un nuovo appartamento basato sulla richiesta validata
+        // (L'implementazione andrà qui)
     }
 
     /**
-     * Display the specified resource.
+     * Mostra la risorsa specificata.
      *
      * @param  \App\Models\Apartment  $apartment
      * @return \Illuminate\Http\Response
      */
     public function show(Apartment $apartment)
     {
-        //
+        // (Possibile implementazione per mostrare i dettagli di un appartamento)
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Mostra il modulo per la modifica della risorsa specificata.
      *
      * @param  \App\Models\Apartment  $apartment
      * @return \Illuminate\Http\Response
      */
     public function edit(Apartment $apartment)
     {
-        //
+        // (Possibile implementazione per modificare un appartamento)
     }
 
     /**
-     * Update the specified resource in storage.
+     * Aggiorna la risorsa specificata nell'archivio.
      *
      * @param  \App\Http\Requests\UpdateApartmentRequest  $request
      * @param  \App\Models\Apartment  $apartment
@@ -73,17 +77,17 @@ class ApartmentController extends Controller
      */
     public function update(UpdateApartmentRequest $request, Apartment $apartment)
     {
-        //
+        // (Possibile implementazione per aggiornare un appartamento)
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Rimuovi la risorsa specificata dall'archivio.
      *
      * @param  \App\Models\Apartment  $apartment
      * @return \Illuminate\Http\Response
      */
     public function destroy(Apartment $apartment)
     {
-        //
+        // (Possibile implementazione per rimuovere un appartamento)
     }
 }
